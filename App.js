c870d801e5640045bfc97501a7ad86e1fdec85c4
Kentaro Sakae <musamusa0607@gmail.com>
2018-02-24 23:35:16 +0900

    SignUpScreenの作成

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import Appbar from './src/components/Appbar';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import LoginScreen from './src/screens/LoginScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpSreen from './src/screens/SignUpScreen';
 
 const styles = StyleSheet.create({
   container: {
@@ -27,7 +28,10 @@ const App = () => (
     {/*
     <MemoEditScreen />
     */}
+    {/*
     <LoginScreen />
+    */}
+    <SignUpSreen />
   </View>
 );
 
---
+++ src/screens/SignUpScreen.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    marginTop: 78,
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 24,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  input: {
+    height: 48,
+    marginBottom: 16,
+    padding: 8,
+    backgroundColor: '#eee',
+    borderWidth: 1,
+    borderColor: '#DDD',
+  },
+  button: {
+    backgroundColor: '#E31676',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    alignSelf: 'center',
+  },
+  buttonTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+});
+
+const SignUpScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>メンバー登録</Text>
+    <TextInput style={styles.input} value="Email Address" />
+    <TextInput style={styles.input} value="Password" />
+    <TouchableHighlight style={styles.button} underlayColor="#C70F66" onPress={() => {}}>
+      <Text style={styles.buttonTitle}>送信する</Text>
+    </TouchableHighlight>
+  </View>
+);
+
+export default SignUpScreen;
