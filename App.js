9afe7441b4dd073b01845db9b6d71c075a60089b
Kentaro Sakae <musamusa0607@gmail.com>
2018-02-28 22:48:11 +0900

    各スクリーンの登録

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import { StackNavigator } from 'react-navigation';
 import MemoListScreen from './src/screens/MemoListScreen';
+import MemoDetailScreen from './src/screens/MemoDetailScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 const App = StackNavigator({
-  Home: {
-    screen: MemoListScreen,
-    navigationOptions: {
-      headerTitle: 'Memot',
-      headerStyle: {
-        backgroundColor: '#265366',
-      },
-      headerTitleStyle: {
-        color: '#fff',
-      },
+  Home: { screen: MemoListScreen },
+  MemoDetail: { screen: MemoDetailScreen },
+  MemoEdit: { screen: MemoEditScreen },
+  Login: { screen: LoginScreen },
+  SignUp: { screen: SignUpScreen },
+}, {
+  navigationOptions: {
+    headerTitle: 'Memot',
+    headerStyle: {
+      backgroundColor: '#265366',
+    },
+    headerTitleStyle: {
+      color: '#fff',
     },
   },
 });
